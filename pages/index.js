fcebb0ad89dd8a97a9ec839b256b804275a1ed27
Maartendeman <103284224+Maartendeman@users.noreply.github.com>
2022-04-08 21:47:42 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,19 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Welkom op mijn website! Wil je meer weten? Laat gerust weten! 
+          Welkom op mijn website! Wil je meer weten? Laat gerust weten! <h2>Een eigen website maken is helamaal niet moeilijk. Wij hebben het hier gratis gedaan via Wix. Via deze makkelijke tool kun je gemakkelijk een website in elkaar flansen.</h2>
+<h2><br></h2>
+<h2>Wij hebben dit onder andere gedaan voor de volgende websites;</h2>
+<h2><br></h2>
+<h2><a href="https://mannnen.nl/" rel="noreferrer noopener" target="_blank">Mannen Website</a></h2>
+<h2><br></h2>
+<h2><a href="https://echtsharon.nl/" rel="noreferrer noopener" target="_blank">Vrouwen Website</a></h2>
+<h2><br></h2>
+<h2><a href="https://support.google.com/profile/151664540" rel="noreferrer noopener" target="_blank">Google Profiel</a></h2>
+<h2><br></h2>
+<h2><a href="https://www.beste-barbecue.nl/" rel="noreferrer noopener" target="_blank">Barbecue Website</a></h2>
+<h2><br></h2>
+<h2>Jij kunt dit ook eenvoudig! Probeer het zelf en kom achter alle mogelijkheden!</h2>
         </p>
       </main>
 
