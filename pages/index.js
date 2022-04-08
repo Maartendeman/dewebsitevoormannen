370201322e43cf6810a15dd07c1c372f12b0892d
Maartendeman <103284224+Maartendeman@users.noreply.github.com>
2022-04-08 21:43:48 +0200

    Update

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome bij mijn website! !" />
         <p className="description">
           Welkom op mijn website! Wil je meer weten? Laat gerust weten! 
         </p>
